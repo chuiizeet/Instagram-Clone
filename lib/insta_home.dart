224a5698fe5b3cbc32b00b76c7db1bf4ed25800d
imac <imac@Dev.local>
2018-09-24 00:35:44 -0500

    Home, Fonts

---
+++ lib/insta_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InstaHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+
+
+
+  }
+}
